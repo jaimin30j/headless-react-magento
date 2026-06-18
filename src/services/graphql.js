4799beaa3363b4a0c9c8ec7f187b54426ab2e02a
Jaimin Patel <jaimin30j@gmail.com>
2026-06-18 16:05:30 +0400

    Add GraphQL handler in graphql.js

---
+++ src/services/graphql.js
@@ -0,0 +1,16 @@
+export default async function handler(req, res) {
+  const response = await fetch(
+    'https://demo2.tyrescart.ae/graphql',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(req.body),
+    }
+  );
+
+  const data = await response.json();
+
+  res.status(200).json(data);
+}
