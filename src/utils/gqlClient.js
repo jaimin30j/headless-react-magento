eac459331c3f4a49aa56aaa727fc8fc5151595e6
Jaimin Patel <jaimin30j@gmail.com>
2026-06-18 15:55:17 +0400

    Refactor gqlClient initialization with environment variable

--- src/utils/gqlClient.js
+++ src/utils/gqlClient.js
@@ -1,13 +1,7 @@
 import { GraphQLClient } from 'graphql-request'
 
-const GRAPHQL_URL = import.meta.env.VITE_GRAPHQL_URL
-    ? `${window.location.origin}/graphql`
-    : import.meta.env.VITE_GRAPHQL_URL
-
-if (!GRAPHQL_URL) {
-    throw new Error('VITE_GRAPHQL_URL is not configured')
-}
-
-const gqlClient = new GraphQLClient(GRAPHQL_URL)
+const gqlClient = new GraphQLClient(
+    import.meta.env.VITE_GRAPHQL_URL
+)
 
 export default gqlClient
